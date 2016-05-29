3af750a93086c10432bd952e9abfb47e040326fc
clwater <gengzhibo@gengzhibo.net>
2016-05-29 22:12:02 +0800

    regular

---
+++ src/regular/regular.java
@@ -0,0 +1,20 @@
+package regular;
+
+import java.util.Vector;
+
+public class regular {
+	
+	static Vector<String> s = new Vector<String>();
+	static Vector<String> r = new Vector<String>();
+	static boolean b ;
+	
+	
+	public static void main(String[] args) {
+			s.add("my name is gzb");
+			r.add("gzb");
+			
+			b = s.get(0).matches(r.get(0));
+			System.out.println(" " + b);
+	}
+
+}
